9c960a7e478a207541eed25ad0d1216a15ec38e7
wagabooga <mattanthu@hotmail.com>
2021-11-09 23:48:32 +0000

    when this commit is applied, using AR's create method created 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,9 @@
 require_relative '../setup'
-
+require_relative '../lib/store'
 puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store_1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store_2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store_3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
