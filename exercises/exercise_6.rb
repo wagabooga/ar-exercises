0dfcec33283d2ab3b82cb26f911555211022ef6a
wagabooga <mattanthu@hotmail.com>
2021-11-10 00:20:51 +0000

    added employees (exercise 6)

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,15 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Matthew", last_name: "Hu", hourly_rate: 70)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 90)
+
+@store2.employees.create(first_name: "Tommy", last_name: "Two-Fingers", hourly_rate: 100)
+
+@store4.employees.create(first_name: "Robes", last_name: "Huyo", hourly_rate: 119)
+
+@store5.employees.create(first_name: "Brett", last_name: "Kouroush", hourly_rate: 120)
+
+@store6.employees.create(first_name: "Jimmy", last_name: "Smith", hourly_rate: 40)
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, :store, presence: true
 
+  validates :hourly_rate, numericality: {greater_than_or_equal_to: 10}
+
+  validates :hourly_rate, numericality: {less_than_or_equal_to: 100}
+
+  belongs_to :store
 end
